cbc987d3b882a080726b5fe547679b3c0a8507d5
emmonina <54905708+emmonina@users.noreply.github.com>
2019-11-06 13:29:57 -0300

    Add files via upload

---
+++ CLASE-9-JS-master/AjaxPami/ej1Texto/index.js
@@ -0,0 +1,36 @@
+
+window.addEventListener('load', function () {
+    let boton = this.document.getElementById('btn');
+    boton.addEventListener('click', traerTexto);
+});
+
+function traerTexto(){
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function(){
+
+            if( xhr.readyState == 4){
+                document.getElementById('loader').innerHTML = "";
+                if( xhr.status == 200){
+                    let info = document.getElementById('info');
+                    info.innerHTML = xhr.responseText;
+                }
+                else{
+                    console.warn( xhr.status + " : " + xhr.statusText );
+                }
+            }
+            else{
+                let span = document.getElementById('loader');
+                span.innerHTML = "<img src='./images/spin.gif'></img>";
+            }
+
+        }
+
+        xhr.open('GET', './documento.txt', true);
+
+        xhr.send();
+
+
+
+}
